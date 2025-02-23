000333b98a7597613ce5aef7813f2b7dbe713d33
swagatasingha40 <swagatasingha40@gmail.com>
2025-02-24 00:15:51 +0530

    first substantial commit

---
+++ reactnativelearn/learnRN/app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const explore = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>explore</Text>
+    </View>
+  );
+};
+
+export default explore;
